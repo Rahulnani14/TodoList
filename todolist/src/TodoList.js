669106cb80bd3064982ff4be0bfc6dccc55ac2a1
Rahul <rahultalari.pohulabs@gmail.com>
2023-09-16 12:56:23 +0530

    updated

---
+++ todolist/src/TodoList.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  function addTask(e) {
+    e.preventDefault();
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, { text: newTask, isComplete: false }]);
+      setNewTask('');
+    }
+  }
+
+  function removeTask(index) {
+    setTasks(tasks.filter((_, i) => i !== index));
+  }
+
+  function markTaskComplete(index) {
+    const newTasks = [...tasks];
+    newTasks[index].isComplete = !newTasks[index].isComplete;
+    setTasks(newTasks);
+  }
+
+  return (
+    <div style={{display:"flex",justifyContent:"center",flexDirection:"column"}}>
+      <h1 style={{display:"flex",justifyContent:"center",color:"blue"}}>Todo List</h1>
+      <div style={{display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column"}}>
+      <form onSubmit={addTask}  >
+        <label>
+          New Task:
+          <input type="text" value={newTask} onChange={e => setNewTask(e.target.value)} />
+        </label>
+        <button type="submit">Add Task</button>
+      </form>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <input type="checkbox" checked={task.isComplete} onChange={() => markTaskComplete(index)} />
+            {task.isComplete ? <del>{task.text}</del> : task.text}
+            <button onClick={() => removeTask(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      </div>
+    </div>
+  );
+}
+
+export default TodoList;
